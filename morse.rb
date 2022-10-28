2674e9ea993209621d6a502286e8a2f2ce64eb0b
techEmprez <choiceosobor@gmail.com>
2022-10-28 21:33:21 +0100

    modified decode string method

--- morse.rb
+++ morse.rb
@@ -37,18 +37,36 @@ def decode_words(word)
   word.split.each do |i|
     translated_word << decode_char(i)
   end
-  
+
   translated_word
 end
 
-# words => strings 
- def decode_str(str)
-  words_arr = str.split('   ')
-  decode_msg = ' '
+def decode_str(str)
+  words_arr = str.split(/   /)
+  decode_msg = ''
 
-  words_arr.each = do |words|
-  decode_msg << ' #{decode_words(words)}'
+  words_arr.each_with_index do |words, i|
+    if i == 0
+      decode_msg << "#{decode_words(words)}"
+    else 
+      decode_msg << " #{decode_words(words)}"
+    end
   end
 
   decode_msg
 end
+
+decode_str('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+# => "a box full of rubies"
+
+
+#Alternative Solution for strings => words
+# # words => strings 
+#  def split_message(message)
+#   splited_message = message.split('   ')
+#   decode_message = ''
+
+#   splited_message.each { |word| decode_message += "#{decode_word(word)} " }
+
+#   decode_message
+# end
