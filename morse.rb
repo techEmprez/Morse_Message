f59336b17a2f434add763798b01fe88db8473a9f
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-10-28 17:44:14 +0200

    fix decode char method and add decode words method

--- morse.rb
+++ morse.rb
@@ -1,42 +1,52 @@
-def decode_char (char)
+def decode_char(char)
   letter_code = {
-  '.-' : 'a',
-  '-...' : 'b',
-  '-.-.' : 'c',
-  '-..' : 'd',
-  '.' : 'e',
-  '..-.' : 'f',
-  '--.' : 'g',
-  '....' : 'h',
-  '..' : 'i',
-  '.---' : 'j',
-  '-.-' : 'k',
-  '.-..' : 'l',
-  '--' : 'm',
-  '-.' : 'n',
-  '---' : 'o',
-  '.--.' : 'p',
-  '--.-' : 'q',
-  '.-.' : 'r',
-  '...' : 's',
-  '-' : 't',
-  '..-' : 'u',
-  '...-' : 'v',
-  '.--' : 'w',
-  '-..-' : 'x',
-  '-.--' : 'y',
-  '--..' : 'z'
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
  }
- puts letter_code[char]
+  letter_code[char]
 end
+
+# words => letter 
+def decode_words(word)
+  translated_word = ''
+  word.split.each do |i|
+    translated_word << decode_char(i)
+  end
+  translated_word
+end
+
 
 
 # words => strings 
 
-def decode_word (str)
-    str.word = str.split
-    return (/   /)
-
+def decode_sentence(str)
+    word = str.split
+   word
+end
     
 
 # codes => str
